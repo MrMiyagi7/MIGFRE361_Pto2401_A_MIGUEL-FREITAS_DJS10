269b3504c4226dc0a0f4ee4b8cf48df665891007
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-23 18:00:14 +0200

    Added layout component to routes

--- BlogsAreUS/src/App.jsx
+++ BlogsAreUS/src/App.jsx
@@ -1,17 +1,15 @@
 import "./App.css";
 import DisplayBlogPosts from "./pages/Blogposts";
-import Header from "../Components/header";
-import Footer from "../Components/footer";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
+import Layout from "../Components/Layout";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
+        <Route element={<Layout />}>
           <Route path="/Home" element={<Home />} />
           <Route path="/Blogs" element={<DisplayBlogPosts />} />
           <Route path="/About" element={<About />} />
