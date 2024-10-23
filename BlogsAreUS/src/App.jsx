9a76938e2ffab281012b64af06cf71776b29d52c
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-23 17:49:40 +0200

    Added routes to blog, home, about, contact pages

--- BlogsAreUS/src/App.jsx
+++ BlogsAreUS/src/App.jsx
@@ -1,16 +1,20 @@
 import "./App.css";
-import DisplayBlogPosts from "./blogPosts";
+import DisplayBlogPosts from "./pages/Blogposts";
 import Header from "../Components/header";
 import Footer from "../Components/footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Header />
-        <DisplayBlogPosts />
-        <Footer />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Blogs" element={<DisplayBlogPosts />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
