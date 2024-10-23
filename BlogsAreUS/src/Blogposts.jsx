797c4852e2daf9322331405e4f943792d48de000
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-23 16:47:30 +0200

    Fetching dummy blogs from api

--- BlogsAreUS/src/Blogposts.jsx
+++ BlogsAreUS/src/Blogposts.jsx
@@ -3,4 +3,8 @@ import { react, useEffect, useState } from "react";
 export default function DisplayBlogPosts() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts");
+  });
 }
