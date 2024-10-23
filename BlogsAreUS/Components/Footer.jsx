d695d1f4d8ab4583f035febad5f7045db7ee94bf
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-23 17:12:36 +0200

    Created Header and Footer components

--- BlogsAreUS/Components/Footer.jsx
+++ BlogsAreUS/Components/Footer.jsx
@@ -1,1 +1,5 @@
 import React from "react";
+
+export default function Footer() {
+  return <footer>&#169; 2024 BlogsAreUS</footer>;
+}
--- BlogsAreUS/Components/Header.jsx
+++ BlogsAreUS/Components/Header.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <img className="nav-logo" src="/favicon.ico"></img>
+        <div className="nav-item-container">
+          <h3 className="nav-item">Home</h3>
+          <h3 className="nav-items">Blogs</h3>
+          <h3 className="nav-items">About us</h3>
+          <h3 className="nav-items">Contact us</h3>
+        </div>
+      </nav>
+    </header>
+  );
+}
