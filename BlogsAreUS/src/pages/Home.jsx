bf94102079d95b3b44efc71e6280c47b5d76b523
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-27 14:58:13 +0200

    Added content to the home page

--- BlogsAreUS/src/pages/Home.jsx
+++ BlogsAreUS/src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Home() {
-  return <h1>This is the home page</h1>;
+  return (
+    <div className="home-content">
+      <div className="hero-image">
+        <h2>Welcome to Blogs Are Us!</h2>
+        <p>Blogs Are Us have just the right blog for you!</p>
+        <button
+          className="hero-button
+        "
+        >
+          CHECK OUT OUR BLOG POSTS
+        </button>
+      </div>
+    </div>
+  );
 }
